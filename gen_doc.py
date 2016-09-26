8eb072a9c1d79bccad917b1b86c441935ce214c3
Guillermo Cornejo S <gmocornejos@gmail.com>
2016-09-25 20:40:37 -0600

    arbol de directorios

--- gen_doc.py
+++ gen_doc.py
@@ -1,12 +1,46 @@
 import re
 import sys
+from pathlib import Path 
 
 func_init = re.compile('\s*\w+\s+\w+\s*\(')
-#func_end = re.compile('[\s\w,.]*\);\s*')
 func_end = re.compile('\);')
 doc_flag = re.compile('\s*//@\s+\w+')
 
+class documentation():
+    def __init__(self, name, docs):
+        self.name = name
+        self.docs = docs
+
+class fuction(documentation):
+    def __init__(self, name, docs):
+        documentation.__init__(self, name, docs)
+
+class datastruct(documentation):
+    def __init__(self, name, docs):
+        documentation.__init__(self, name, docs)
+
+class source_file(documentation):
+    def __init__(self, path, name, docs):
+        documentation.__init__(self, name, docs)
+        self.functions = list()
+        self.datastrucs = list()
+
+def build_tree(dirs):
+    tree = dict()
+    for d in dirs:
+        l = [s for s in d.iterdir() if s.is_dir()]
+        tree.update({d:build_tree(l)})
+    return tree
+
+def print_tree(dirs, level):
+    for k, v in dirs.items():
+        print('-'*level + k.name)
+        print_tree(v, level+1)
+
 def main():
+    directories_tree = build_tree( [Path(x) for x in sys.argv[1:] ])
+    print_tree(directories_tree, 0)
+'''    
     file = open(sys.argv[1], 'r')
 
     for line in file:
@@ -15,7 +49,7 @@ def main():
             print(' '.join(line[2:]) )
             print('\n')
             print(''.join(func_collect(file)))
-
+'''
 def func_collect(file):
     function = None
     for line in file:
